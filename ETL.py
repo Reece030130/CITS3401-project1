50a6cab09aa16b23bdf7f98e9c6f1be59bc583bf
Reece0301 <22857062@student.uwa.edu.au>
2025-03-27 06:47:54 +0800

    Update 1.0

--- ETL.py
+++ ETL.py
@@ -4,51 +4,18 @@ import polars as pl # type: ignore
 def file_loading(filename: str, sheet_name: str) -> pl.DataFrame:
     df = pl.read_excel(filename, sheet_id=0)[sheet_name]
     new_columns = df.row(1)
-    sheet_name = df.rename({old: new for old, new in zip(df.columns, new_columns)})
+    sheet_name = df.rename({old: new for old, new in zip(df.columns, new_columns)}).slice(2)
     return sheet_name
-
-
-def dim_date(sheet_name: pl.DataFrame):
-    dim_date_df = sheet_name[['Day of week', 'Dayweek', 'Month', 'Year']].unique()
-    dim_date_df = dim_date_df.filter(
-        pl.fold(
-            acc=True,  # Start with True (keep rows by default)
-            function=lambda acc, col: acc & (col != -9),  # Keep rows where all columns are NOT -9
-            exprs=[pl.col(col) for col in dim_date_df.columns]  # Apply to all columns
-        )
-    )
-    dim_date_df = dim_date_df.sort(["Year", "Month"])
-    dim_date_df = dim_date_df.with_columns(
-        pl.format("Date{}", pl.arange(1, len(dim_date_df) + 1)).alias("DateId")
-    )
-    dim_date_df = dim_date_df.select(["DateId"] + [col for col in dim_date_df.columns if col != "DateId"])
-
-    dim_date_df.write_csv("dim_dates.csv")
-    pass
-
-
-def dim_geography(sheet_name: pl.DataFrame):
-    dim_geography_df = sheet_name[['State', 'SA4 Name 2021', 'National LGA Name 2021',
-                                   'National Remoteness Areas']].unique()
-    dim_geography_df = dim_geography_df.with_columns(
-        pl.format("Geo{}", pl.arange(1, len(dim_geography_df) + 1)).alias("GeoId")
-    )
-    dim_geography_df = dim_geography_df.select(["GeoId"] + [col for col in dim_geography_df.columns if col != "GeoId"])
-    dim_geography_df.rename({'SA4 Name 2021': 'SA4', 'National LGA Name 2021': 'National LGA'})
-    dim_geography_df.write_csv("dim_geography.csv")
-    pass
-
-
-def dim_fatalities(sheet_name: pl.DataFrame):
-    dim_fatalities_df = sheet_name[["Age Group", "Age", "Gender"]].unique()
-    dim_fatalities_df = dim_fatalities_df.with_columns(
-        pl.format("Fatalities{}", pl.arange(1, len(dim_fatalities_df) + 1)).alias("FatalityId")
-    )
-    dim_fatalities_df = dim_fatalities_df.select(["FatalityId"]+ [col for col in dim_fatalities_df.columns if col != "FatalityId"])
-    dim_fatalities_df.write_csv("dim_fatalities.csv")
 
 def dim_time(sheet_name: pl.DataFrame):
     dim_time_df = sheet_name[['Time of Day','Time']].unique()
+    dim_time_df = dim_time_df.filter(
+        pl.fold(
+            acc=True,  # Start with True (keep rows by default)
+            function=lambda acc, col: acc & (col != -9),  # Keep rows where all columns are NOT -9
+            exprs=[pl.col(col) for col in dim_time_df.columns]  # Apply to all columns
+        )
+    )
     dim_time_df = dim_time_df.with_columns(
         pl.format("Time{}", pl.arange(1, len(dim_time_df)+1)).alias("TimeId"),
         pl.col("Time").str.extract(r"(\d{2}:\d{2}:\d{2})").str.strptime(pl.Time, "%H:%M:%S")
@@ -56,7 +23,21 @@ def dim_time(sheet_name: pl.DataFrame):
     dim_time_df = dim_time_df.select(["TimeId"]+ [col for col in dim_time_df.columns if col != "TimeId"])
     dim_time_df.write_csv("dim_time_df.csv")
 
-
-
-
-
+def dim_table_creation(sheet_name: pl.DataFrame, dimension_variable: list, short_list: list |None, dimension_primary_key: str, filter_value: str|int, convert_name: str, csv_name: str):
+    dim_table_df = sheet_name.select(dimension_variable).unique()
+    dim_table_df = dim_table_df.filter(
+        pl.fold(
+            acc=True,
+            function=lambda acc, col: acc & (col != filter_value),
+            exprs=[pl.col(col) for col in dim_table_df.columns]
+        )
+    )
+    if short_list:
+        dim_table_df.sort(short_list)
+    dim_table_df = dim_table_df.with_row_count(name=dimension_primary_key)
+    dim_table_df = dim_table_df.with_columns(
+        (pl.lit(convert_name) + (pl.col(dimension_primary_key) + 1).cast(pl.Utf8)).alias(dimension_primary_key)
+    )
+    dim_table_df = dim_table_df.select([dimension_primary_key] + [col for col in dim_table_df.columns if col != dimension_primary_key])
+    dim_table_df.write_csv(csv_name)
+    return None
