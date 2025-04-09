6136ea927790ffe7a75f9c0ba8a5aa42d79964d3
Reece0301 <22857062@student.uwa.edu.au>
2025-04-10 01:28:55 +0800

    final update

--- ETL.py
+++ ETL.py
@@ -7,7 +7,7 @@ def file_loading(filename: str, sheet_name: str) -> pl.DataFrame:
     df = df.rename({old: new for old, new in zip(df.columns, new_columns)}).slice(2)
     return df
 
-def dim_table_creation(sheet_name: pl.DataFrame, dimension_variable: list, sort_list: list |None, extract : dict | None, dimension_primary_key: str, filter_value: list[str|int], convert_name: str, csv_name: str):
+def dim_table_creation(sheet_name: pl.DataFrame, dimension_variable: list, sort_list: list |None, extract : dict | None, dimension_primary_key: str, filter_value: list[str|int], castint_value: list[str] , convert_name: str, csv_name: str):
     dim_table_df = sheet_name.select(dimension_variable).unique()
     
     for i in filter_value:     
@@ -30,9 +30,19 @@ def dim_table_creation(sheet_name: pl.DataFrame, dimension_variable: list, sort_
         (pl.lit(convert_name) + (pl.col(dimension_primary_key) + 1).cast(pl.Utf8)).alias(dimension_primary_key)
     )
     dim_table_df = dim_table_df.select([dimension_primary_key] + [col for col in dim_table_df.columns if col != dimension_primary_key])
+    if castint_value:
+        for i in castint_value:
+            dim_table_df = dim_table_df.with_columns(
+                pl.when(pl.col(i).str.contains(r"^\d+$"))
+                .then(pl.col(i).cast(pl.Int64, strict=False))
+                .otherwise(None)  # or .otherwise(pl.lit("Unknown")) if you want to keep the original text
+                .alias(i)
+            )
     dim_table_df.write_csv(csv_name)
     return dim_table_df
 
 def fact_table_creation(dim_tables: pl.DataFrame, original_table: pl.DataFrame, dim_tables_variable: list[str]):
     fact_table = original_table.join(dim_tables, on = dim_tables_variable).drop(dim_tables_variable)
     return fact_table
+
+
--- __init__.py
+++ __init__.py
@@ -4,9 +4,19 @@ if __name__ == '__main__':
     sheet_name = file_loading("bitre_fatal_crashes_dec2024.xlsx","BITRE_Fatal_Crash")
     sheet_name2 =file_loading("bitre_fatalities_dec2024.xlsx", "BITRE_Fatality")
     sheet_name2 = sheet_name2.join(sheet_name.select(["Crash ID", "Number Fatalities"]), on="Crash ID", how="left")
-    print(sheet_name2)
-    sheet_name = sheet_name.rename({'SA4 Name 2021': 'Sa4 Name21', 'State':'Ste Name21', 'National LGA Name 2021': 'Lga Name21'})
-    sheet_name2 = sheet_name2.rename({'SA4 Name 2021': 'Sa4 Name21', 'State':'Ste Name21', 'National LGA Name 2021': 'Lga Name21'})
+    geo_name = {'SA4 Name 2021': 'Sa4 Name21', 'State':'Ste Name21', 'National LGA Name 2021': 'Lga Name21'}
+    sheet_name = sheet_name.rename(geo_name)
+    sheet_name2 = sheet_name2.rename(geo_name)
+    sheet_name2 = sheet_name2.with_columns([
+        pl.when(pl.col(col) == "-9")
+        .then(None)
+        .otherwise(pl.col(col))
+        .alias(col)
+        for col in sheet_name2.columns
+    ])
+    sheet_name2 = sheet_name2.fill_null("Unknown")
+    sheet_name2.write_csv("cleaned_data.csv")
+
     replacement_dict = {
     'NSW': 'New South Wales',
     'NT': 'Northern Territory',
@@ -38,26 +48,25 @@ if __name__ == '__main__':
     # create fatalityID and perpare for the fact table.
     dim_table_list =[
         # dimension date
-        [sheet_name, dim_tables_variables[0], ["Year","Month"] , None, "DateID", ["-9","Unknown"], "Date", "dim_date.csv"],  
+        [sheet_name, dim_tables_variables[0], ["Year","Month"] , None, "DateID", ["-9","Unknown"], ["Month","Year"], "Date", "dim_date.csv"],  
         # dimension Geography
-        [sheet_name, dim_tables_variables[1], None, None, "GeoID", ["-9","Unknown"], "Geo", "dim_geography.csv"],
+        [sheet_name2, dim_tables_variables[1], None, None, "GeoID", [], [],"Geo", "dim_geography.csv"],
         # dimension fatalities
-        [sheet_name2, dim_tables_variables[2], None, None, "FatalityPersonalInfoId",["-9","Unknown"], "FatalitiesPI", "dim_fatalitiesPersonalInfo.csv"],
+        [sheet_name2, dim_tables_variables[2], None, None, "FatalityPersonalInfoId",["-9","Unknown"],["Age"], "FatalitiesPI", "dim_fatalitiesPersonalInfo.csv"],
         # dimension crash
-        [sheet_name, dim_tables_variables[3],None, None, "CrashID", ["-9","Unknown"],"Crash", "dim_crash.csv"],
+        [sheet_name, dim_tables_variables[3],None, None, "CrashID", [],[],"Crash", "dim_crash.csv"],
         # dimension road
-        [sheet_name2, dim_tables_variables[4], None, None, "RoadId", ["-9","Unknown"], "Road", "dim_road_df.csv"],
+        [sheet_name2, dim_tables_variables[4], None, None, "RoadId", ["-9","Unknown"],[], "Road", "dim_road_df.csv"],
         # dimension vehicle
-        [sheet_name,dim_tables_variables[5], None, None, "VehicleId", ["-9","Unknown"], "Vehicle", "dim_vehicle_df.csv"],
+        [sheet_name2,dim_tables_variables[5], None, None, "VehicleId", [],[], "Vehicle", "dim_vehicle_df.csv"],
         # dimension Time
-        [sheet_name2, dim_tables_variables[6], None, {"Time": r"(\d{2}:\d{2}:\d{2})"},"TimeID",["-9","Unknown"],"Time", "dim_time.csv"],
+        [sheet_name2, dim_tables_variables[6], None, {"Time": r"(\d{2}:\d{2}:\d{2})"},"TimeID",["-9","Unknown"],[],"Time", "dim_time.csv"],
         # dimension hoilday
-        [sheet_name,dim_tables_variables[7], None, None, "HolidayID", ["-9","Unknown"], "Holiday", "dim_holiday.csv"]
+        [sheet_name,dim_tables_variables[7], None, None, "HolidayID", ["-9","Unknown"],[], "Holiday", "dim_holiday.csv"]
     ]
-    for [sheetname, dimension_variable, short_list, extract, dimension_primary_key, filter_value, convert_name, csv_name] in dim_table_list:
-        dim_tables.append(dim_table_creation(sheetname, dimension_variable, short_list, extract, dimension_primary_key, filter_value, convert_name, csv_name))
-    fatal = dim_table_creation(sheet_name2, sheet_name2.columns ,None, {"Time": r"(\d{2}:\d{2}:\d{2})"}, "FatalityId",["-9", "Unknown"], "Fatality", "BITRE_Fatality.csv")
-    print(fatal)
+    for [sheetname, dimension_variable, short_list, extract, dimension_primary_key, filter_value, castint_value ,convert_name, csv_name] in dim_table_list:
+        dim_tables.append(dim_table_creation(sheetname, dimension_variable, short_list, extract, dimension_primary_key, filter_value, castint_value, convert_name, csv_name))
+    fatal = dim_table_creation(sheet_name2, sheet_name2.columns ,None, {"Time": r"(\d{2}:\d{2}:\d{2})"}, "FatalityId",[],["Year","Month", "Age"],  "Fatality", "fact_table.csv")
     for i in range(len(dim_tables_variables)):
         fatal = fact_table_creation(dim_tables[i], fatal, dim_tables_variables[i])
     fatal.write_csv("fact_table.csv")
